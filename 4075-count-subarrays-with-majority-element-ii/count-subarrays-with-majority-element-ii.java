dea4b7312cfd42176498fa73b9d0c8cc89b5b7ed
ad-aditya01 <adityaazm001@gmail.com>
2026-06-26 19:02:50 +0530

    Time: 47 ms (37.71%) | Memory: 116.2 MB (18.03%) - LeetSync

---
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+
+    class Fenwick {
+        int[] bit;
+
+        Fenwick(int n) {
+            bit = new int[n + 2];
+        }
+
+        void add(int idx, int val) {
+            while (idx < bit.length) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+
+        int n = nums.length;
+
+        int[] prefix = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        int[] sorted = prefix.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int rank = 1;
+
+        for (int x : sorted) {
+            if (!map.containsKey(x)) {
+                map.put(x, rank++);
+            }
+        }
+
+        Fenwick bit = new Fenwick(rank);
+
+        long ans = 0;
+
+        for (int x : prefix) {
+
+            int idx = map.get(x);
+
+            ans += bit.query(idx - 1);
+
+            bit.add(idx, 1);
+        }
+
+        return ans;
+    }
+}
