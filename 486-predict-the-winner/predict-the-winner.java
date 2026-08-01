4980c33ac046b8173d1cee2c9282d814e949ec69
ad-aditya01 <adityaazm001@gmail.com>
2026-08-02 02:36:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (23.58%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    Integer[][] dp;
+
+    public int solve(int i, int j, int[] nums) {
+
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        int left = nums[i] - solve(i + 1, j, nums);
+        int right = nums[j] - solve(i, j - 1, nums);
+
+        return dp[i][j] = Math.max(left, right);
+    }
+
+    public boolean predictTheWinner(int[] nums) {
+
+        int n = nums.length;
+        dp = new Integer[n][n];
+
+        return solve(0, n - 1, nums) >= 0;
+    }
+}
