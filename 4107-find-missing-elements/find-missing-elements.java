3404d65a73669cc8cb9bdc07c7603391a90fedf9
ad-aditya01 <adityaazm001@gmail.com>
2026-08-04 13:58:59 +0530

    Time: 5 ms (43.94%) | Memory: 47.2 MB (25.78%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int n=nums.length;
+        // int mini=Integer.MAX_VALUE;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<mini){
+        //         mini=nums[i];
+        //     }
+        // }
+        // int maxi=Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>max){
+        //         max=nums[i];
+        //     }
+        // }
+        // for(int i=mini;i<maxi;i++){
+        //     if()
+        // }
+        
+        Arrays.sort(nums);
+        ArrayList<Integer> list=new ArrayList();
+        int a=nums[0];
+        for(int i=0;i<n;i++){
+            while(a<nums[i]){
+                list.add(a);
+                a++;
+            }
+            if(nums[i]==a){
+                a++;
+            }
+        }
+        return list;
+    }
+}
