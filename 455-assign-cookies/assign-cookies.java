e4ca546ae156aaa88ba5ff0861a640504c681e16
ad-aditya01 <adityaazm001@gmail.com>
2026-08-05 10:33:09 +0530

    Time: 15 ms (59.86%) | Memory: 50.9 MB (97.62%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l=0,r=0;
+        int count=0;
+
+        while(l<g.length && r<s.length){
+            if(g[l]<=s[r]){
+                count++;
+                l++;
+                r++;
+            }else{
+            r++;
+            }
+        }
+        return count;
+       
+        
+    }
+}
