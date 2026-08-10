944acda9a64a1c749f8ab4f6296cdcc6a043001f
ad-aditya01 <adityaazm001@gmail.com>
2026-08-10 15:46:41 +0530

    Time: 18 ms (27.34%) | Memory: 45.4 MB (13.78%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -9,7 +9,7 @@ class Solution {
 
         int maxi=1;
         int lastIndex=0;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[i]%nums[j]==0 && dp[j]+1>dp[i]){
                     dp[i]=dp[j]+1;
