f827f9cbca24167e00c41654c194aca0e43fda84
ad-aditya01 <adityaazm001@gmail.com>
2026-08-26 21:21:26 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (75.84%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,44 +1,85 @@
+// class Solution {
+//     public String shortestBeautifulSubstring(String s, int k) {
+
+//         HashMap<Character, Integer> map = new HashMap<>();
+
+//         int minLen = Integer.MAX_VALUE;
+//         int i = 0;
+//         int count = 0;
+//         String ans = "";
+
+//         for (int j = 0; j < s.length(); j++) {
+//             char ch = s.charAt(j);
+
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+//             if (ch == '1') {
+//                 count++;
+//             }
+//             while (count == k) {
+//                 int len = j - i + 1;
+
+//                 if (len < minLen) {
+//                     minLen = len;
+//                     ans = s.substring(i, j + 1);
+//                 }
+//                 else if (len == minLen) {
+//                     String current = s.substring(i, j + 1);
+
+//                     if (current.compareTo(ans) < 0) {
+//                         ans = current;
+//                     }
+//                 }
+//                 char remove = s.charAt(i);
+
+//                 map.put(remove, map.get(remove) - 1);
+
+//                 if (map.get(remove) == 0) {
+//                     map.remove(remove);
+//                 }
+
+//                 if (remove == '1') {
+//                     count--;
+//                 }
+
+//                 i++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        int minLen = Integer.MAX_VALUE;
         int i = 0;
         int count = 0;
+        int minLen = Integer.MAX_VALUE;
         String ans = "";
 
         for (int j = 0; j < s.length(); j++) {
-            char ch = s.charAt(j);
 
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-
-            if (ch == '1') {
+            if (s.charAt(j) == '1') {
                 count++;
             }
+
             while (count == k) {
+
                 int len = j - i + 1;
 
                 if (len < minLen) {
                     minLen = len;
                     ans = s.substring(i, j + 1);
-                }
+                } 
                 else if (len == minLen) {
                     String current = s.substring(i, j + 1);
 
-                    if (current.compareTo(ans) < 0) {
+                    if (ans.equals("") || current.compareTo(ans) < 0) {
                         ans = current;
                     }
                 }
-                char remove = s.charAt(i);
 
-                map.put(remove, map.get(remove) - 1);
-
-                if (map.get(remove) == 0) {
-                    map.remove(remove);
-                }
-
-                if (remove == '1') {
+                if (s.charAt(i) == '1') {
                     count--;
                 }
 
