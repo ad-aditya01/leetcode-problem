75b40e5249933738020711f51646a6b00c913feb
ad-aditya01 <adityaazm001@gmail.com>
2026-09-20 21:43:03 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (18.31%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,14 +1,25 @@
-class Solution {
-    public int reverseDegree(String s) {
+// class Solution {
+//     public int reverseDegree(String s) {
 
-        int result=0;
-        for(int i=0;i<s.length();i++){
-            int ans=26-(s.charAt(i)-'a');
+//         int result=0;
+//         for(int i=0;i<s.length();i++){
+//             int ans=26-(s.charAt(i)-'a');
 
-            result=result+ans*(i+1);
-        }
-        return result;
+//             result=result+ans*(i+1);
+//         }
+//         return result;
 
         
+//     }
+// }
+
+class Solution {
+    public int reverseDegree(String s) {
+        int result = 0;
+        int index = 1;
+        for (char c : s.toCharArray()) {
+            result += ('z' - c + 1) * index++;
+        }
+        return result;
     }
 }
